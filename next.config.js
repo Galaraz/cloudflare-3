f01562c668b695b11d7dc03122e41edb15009ad2
galaraz <jvictor.si.2013@gmail.com>
2023-05-15 10:52:37 -0400

    adiçao de estilo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
   reactStrictMode: true,
+  
+  swcMinify: true,
+  images: {
+    domains : ['https://static-dev.infoimoveis.com.br'],
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static-dev.infoimoveis.com.br',
+      },
+    ],
+  },
+  api: {
+    bodyParser: {
+      sizeLimit: '1mb',
+    },
+    externalResolver: true,
+  },
 }
 
 module.exports = nextConfig
