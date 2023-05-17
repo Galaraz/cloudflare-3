40249e784d40402639a1e15d598d791f0233d9b0
galaraz <jvictor.si.2013@gmail.com>
2023-05-17 09:59:14 -0400

    teste-04

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
 
   reactStrictMode: true,
   swcMinify: true,
-  experimental: { runtime: 'experimental-edge'},
+  
   images: {
     domains : ['https://static-dev.infoimoveis.com.br'],
     dangerouslyAllowSVG: true,
