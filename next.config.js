5c52498d6fdcd97b114e6db92720b5a0484304b2
galaraz <jvictor.si.2013@gmail.com>
2023-05-19 14:28:35 -0400

    teste na borda

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
 
   reactStrictMode: true,
-  experimental: { runtime: 'edge'},
+  experimental: {
+    runtime: "experimental-edge",
+    
+ },
+  
   swcMinify: true,
   images: {
     domains : ['https://static-dev.infoimoveis.com.br'],
